86b7aba2a4666ed4c95177f45a94048f0b829105
Ridam Hazra <rhazra0602@gmail.com>
2022-03-15 11:45:31 +0530

    Time: 4 ms (86.15%), Space: 10 MB (48.16%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> result;
+        
+        for(int i(0); i<nums.size(); i+=2)
+        {
+            result.insert(result.end(), nums[i], nums[i+1]);
+        }
+        
+        return result;
+    }
+};
