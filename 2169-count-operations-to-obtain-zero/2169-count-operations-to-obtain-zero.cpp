b8f0423ac24c1783cae49e236dd4e9b0243f16da
Ridam Hazra <rhazra0602@gmail.com>
2022-05-27 13:16:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (29.45%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countOperations(int a, int b) {
+        int res = 0;
+        while (min(a, b) > 0) 
+        {
+            if (a > b)
+            {
+                swap(a, b);
+            }
+            
+            res += b / a;
+            b %= a;
+        }
+        
+        return res;
+    }
+};
