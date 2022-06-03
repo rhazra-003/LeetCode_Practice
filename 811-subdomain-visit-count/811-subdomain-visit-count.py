91d46249a0bf32b0d08c6e35a0e15c0c154a515c
Ridam Hazra <rhazra0602@gmail.com>
2022-06-03 19:11:25 +0530

    Time: 86 ms (30.94%), Space: 13.9 MB (81.46%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        count = collections.Counter()
+        
+        for cd in cpdomains:
+            n, s = cd.split()
+            count[s] += int(n)
+            
+            for i in range(len(s)):
+                if s[i] == '.':
+                    count[s[i + 1:]] += int(n)
+        
+        return ["%d %s" % (count[k], k) for k in count]
