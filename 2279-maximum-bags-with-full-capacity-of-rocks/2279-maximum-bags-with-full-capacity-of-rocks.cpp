fd5357109219583adee750ec778bba1257e403dc
Ridam Hazra <rhazra0602@gmail.com>
2022-07-03 18:27:42 +0530

    Time: 329 ms (31.03%), Space: 89.7 MB (15.24%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int k) {
+        vector<int> count;
+        int i, n = rocks.size();
+            
+        for (int i = 0; i < n; ++i) 
+        {
+            count.push_back(capacity[i] - rocks[i]);
+        }
+            
+        sort(count.begin(), count.end());
+            
+        for (i = 0; i < n && k >= count[i]; ++i)
+        {
+            k -= count[i];
+        }
+            
+        return i;
+    }
+};
+
