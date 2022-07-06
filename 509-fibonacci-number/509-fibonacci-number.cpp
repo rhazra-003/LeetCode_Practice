8caa6ba76456f6084646c764c7ddb694aec389ce
Ridam Hazra <rhazra0602@gmail.com>
2022-07-06 10:38:49 +0530

    Time: 31 ms (5.64%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        int d;
+        
+        if(n == 0 || n == 1)
+        {
+            return n;
+        }
+        
+        d = fib(n-1) + fib(n-2);
+        
+        return d;
+    }
+};
