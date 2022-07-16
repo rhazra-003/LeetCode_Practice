133abe7438c25648adef884106eae6db8754954f
Ridam Hazra <rhazra0602@gmail.com>
2022-07-16 22:40:16 +0530

    Time: 3 ms (37.80%), Space: 6 MB (29.55%) - LeetHub

--- 878-nth-magical-number/878-nth-magical-number.cpp
+++ 878-nth-magical-number/878-nth-magical-number.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
+    
     int nthMagicalNumber(int N, int A, int B) {
-        long L = lcm(A, B), C = (L/A) + (L/B) - 1, k = (N-1) / C, ans = 0;
+        long LCM = lcm(A, B), L = min(A, B), R = N*L, mid, ans;
         
-        for(int i = 1, j = 1, magicalNeeded = N - k*C; magicalNeeded; magicalNeeded--) 
+        auto magicNumbersLessEqual = [&](long x) 
+        { 
+            return (x/A) + (x/B) - (x/LCM); 
+        };
+        
+        while(L <= R) 
         {
-            if(A*i < B*j) 
+            mid = L + (R-L) / 2;
+            
+            if(magicNumbersLessEqual(mid) < N) 
             {
-                ans = A * i++;
+                L = mid + 1;   // magical numbers <= mid are less than required, so discard search space to left of mid
             }
             else 
             {
-                ans = B * j++;
-            }
+                R = mid - 1, ans = mid;                      // mark mid as the potential answer & search in [L, mid-1]
+            }    
         }
         
-        return (k*L + ans) % MOD;
+        return ans % MOD;
     }
 };
