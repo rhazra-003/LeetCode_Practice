2ae9c545ea00a6ea1c4849105e1aab1f974f97c2
Ridam Hazra <rhazra0602@gmail.com>
2022-07-22 16:39:33 +0530

    Time: 55 ms (94.04%), Space: 28.2 MB (48.02%) - LeetHub

---
+++ 561-array-partition/561-array-partition.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int sum=0;
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i%2 == 0)
+            {
+                sum = sum + nums[i];
+                i++;
+            }
+        }
+        
+        return sum;
+    }
+};
