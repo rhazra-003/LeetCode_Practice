2d1667873516416fa309ffbb4ebc5c39a66643d6
Ridam Hazra <rhazra0602@gmail.com>
2022-07-22 19:09:38 +0530

    Time: 15 ms (80.47%), Space: 6 MB (31.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0))
+        {
             return false;
+        }
         
         int z = 0;
+        
         while (x > z)
         {
             z = z * 10 +  x % 10;
