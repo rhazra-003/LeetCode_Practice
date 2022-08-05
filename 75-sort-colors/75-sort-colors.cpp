feb831d8a0f75a5f2802ac167b75c513629d65e5
Ridam Hazra <rhazra0602@gmail.com>
2022-08-06 00:38:09 +0530

    Time: 4 ms (45.02%), Space: 8.2 MB (90.52%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,23 +1,50 @@
+// Dutch National Flag Algo / 3  pointers Approach
+
+// class Solution { 
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int i = 0, j = i, k = nums.size() - 1;
+        
+//         while(j <= k)
+//         {
+//             if(nums[j] == 0) 
+//             {
+//                 swap(nums[i++], nums[j++]);
+//             }
+            
+//             else if(nums[j] == 1) 
+//             {
+//                 j++;
+//             }
+            
+//             else 
+//             {
+//                 swap(nums[k--], nums[j]);
+//             }
+//         }
+//     }
+// };
+
 class Solution { 
 public:
     void sortColors(vector<int>& nums) {
-        int i = 0, j = i, k = nums.size() - 1;
+        int low = 0, mid = 0, high = nums.size() - 1;
         
-        while(j <= k)
+        while(mid <= high)
         {
-            if(nums[j] == 0) 
+            switch(nums[mid])
             {
-                swap(nums[i++], nums[j++]);
-            }
-            
-            else if(nums[j] == 1) 
-            {
-                j++;
-            }
-            
-            else 
-            {
-                swap(nums[k--], nums[j]);
+                case 0:
+                    swap(nums[low++], nums[mid++]);
+                    break;
+                    
+                case 1:
+                    mid++;
+                    break;
+                    
+                case 2:
+                    swap(nums[mid], nums[high--]);
+                    break;
             }
         }
     }
