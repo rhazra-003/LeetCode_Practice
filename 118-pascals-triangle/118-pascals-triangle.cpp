1e653189f213d01b50d6a9c17ae8c4ff988a0f00
Ridam Hazra <rhazra0602@gmail.com>
2022-08-06 13:43:56 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int> > generate(int numRows) {
-        vector<vector<int>> r(numRows);
+        vector<vector<int>> r(numRows); // n number of columns
 
-        for (int i = 0; i < numRows; i++) 
+        for (int i = 0; i < numRows; i++) // traversal in row
         {
-            r[i].resize(i + 1);
-            r[i][0] = r[i][i] = 1;
+            r[i].resize(i + 1); // resized to increase as number of columns is equal to number of row
+            r[i][0] = r[i][i] = 1; // default 1's in extrems
   
-            for (int j = 1; j < i; j++)
+            for (int j = 1; j < i; j++) // traversal in column
             {
-                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j]; // 1+2=3 or 1+3=4
             }
         }
         
-        return r;
+        return r; // return of triangle
     }
 };
