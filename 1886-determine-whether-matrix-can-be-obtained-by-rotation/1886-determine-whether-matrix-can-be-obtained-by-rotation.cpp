e31c6e82c6f8986ac06e12573cb8fb069ef88583
Ridam Hazra <rhazra0602@gmail.com>
2022-08-12 09:03:44 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (74.37%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        if(mat == target){
+            return true;
+        }
+        
+        int degree = 3;
+        
+        while(degree--)
+        {
+            for(int i=0; i<mat.size(); i++)
+            {
+                // transpose of the given matrix
+                for(int j=i+1; j<mat.size(); j++)
+                {
+                    swap(mat[i][j], mat[j][i]);
+                }
+                // reverse the rows
+                reverse(mat[i].begin(), mat[i].end());
+            }
+            
+            if(mat == target) 
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
