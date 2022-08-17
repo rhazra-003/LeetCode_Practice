3efc6001d4383b0bcfc61998aaddaea5330cb0fe
Ridam Hazra <rhazra0602@gmail.com>
2022-08-17 23:59:52 +0530

    Time: 43 ms (17.42%), Space: 19.6 MB (59.64%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,19 @@
+// Hashmap method
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        
+        for(auto i : nums)
+        {
+            mp[i]++;
+            
+            if(mp[i] > nums.size()/2)
+            {   
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
