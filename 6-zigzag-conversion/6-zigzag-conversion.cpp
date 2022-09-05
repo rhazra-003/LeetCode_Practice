4c092107f458373660a1fc6791e5ceb9cfd627e3
Ridam Hazra <rhazra0602@gmail.com>
2022-09-05 09:15:12 +0530

    Time: 8 ms (89.21%), Space: 10.7 MB (39.34%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1 ) 
+        {
+            return s;
+        }
+        
+        vector<string> res(numRows);
+        int row = 0;
+        int increase =- 1;
+        
+        for(int i = 0; i<s.size(); ++i)
+        {
+            // every time at turning point, we change the increase direction
+            if(i % (numRows-1) == 0) 
+            {
+                increase *= -1;
+            }
+            
+            res[row].push_back(s[i]);
+            row += increase;
+        }
+        
+        string ret;
+        
+        for(const auto& str:res)
+        {
+            ret += str;
+        }
+        
+        return ret;
+    }
+};
