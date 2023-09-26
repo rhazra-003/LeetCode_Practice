7f4582366d8ffb202afee6ce213d4b331a324062
Ridam Hazra <rhazra0602@gmail.com>
2023-09-26 21:44:28 +0530

    Time: 7 ms (73.37%), Space: 6.2 MB (19.43%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+        
+        int z = 0;
+        
+        while (x > z)
+        {
+            z = z * 10 +  x % 10;
+            
+            x = floor(x/10);
+        }
+            
+        return x == z || x == floor(z / 10);
+    }
+};
